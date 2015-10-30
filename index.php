3364a47204d556c7e1f60d37786937181898f537
Nandini Sridharan <nandini9590@gmail.com>
2015-10-30 00:29:32 -0500

    making changes

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <?php session_start(); ?>
 <html>
-<head>
-<title>Index page </title>
+<head><title>Hello app</title>
+<meta charset="utf-8">
 </head>
 <body>
 
-<!-- The data encoding type, enctype, MUST be specified as below -->
+
 <form enctype="multipart/form-data" action="result.php" method="POST">
     
+    <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+   
     Send this file: <input name="userfile" type="file" /><br />
+<br>
+<br>
 Enter Email of user: <input type="email" name="useremail"><br />
+<br>
+<br>
 Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
+<br>
+<br>
 
-
-<input type="submit" value="Submit" />
+<input type="submit" value="Send File" />
 </form>
-<hr />
-<!-- The data encoding type, enctype, MUST be specified as below -->
+
+
 <form enctype="multipart/form-data" action="gallery.php" method="POST">
-  
+ <br>
 Enter Email of user for gallery to browse: <input type="email" name="email">
 <input type="submit" value="Load Gallery" />
 </form>
+
+
 </body>
 </html>

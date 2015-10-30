c9cdf50fa45c0912bf329830d3f3b69f835f8962
Nandini Sridharan <nandini9590@gmail.com>
2015-10-29 23:43:38 -0500

    creating a table

--- dbcreate.php
+++ dbcreate.php
@@ -1,21 +1,22 @@
 
 
+
+
 <?php
-//conection: 
-echo "Hello world"; 
-$link = mysqli_connect("itmo544jrhdb","controller","ilovebunnies","3306") or die("Error " . mysqli_error($link)); 
+$sqli_conn = new mysqli("project1db.c7brzs9x3acd.us-west-2.rds.amazonaws.com","nandini","nandinipwd","Project1db",3306) or die("Error " . mysqli_error($link)); 
 
-echo "Here is the result: " . $link;
-
-
-$sql = "CREATE TABLE comments 
+$res = $sqli_conn->query("CREATE TABLE Projectrec
 (
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500)
-)";
-
-$con->query($sql);
-
+id int NOT NULL AUTO_INCREMENT,
+uname VARCHAR(20),
+email VARCHAR(20),
+phone VARCHAR(20),
+raws3url VARCHAR(256),
+finisheds3url VARCHAR(256),
+jpegfilename VARCHAR(256),
+state tinyint(3) CHECK(state IN (0,1,2)),
+DateTime timestamp,
+PRIMARY KEY (id)
+)");
+$permission=shell_exec("chmod 600 setup.php");
 ?>

919677f733b069b0cfc3beb10b04509b73bd799d
Nandini Sridharan <nandini9590@gmail.com>
2015-10-30 20:05:00 -0500

    db changes

--- gallery.php
+++ gallery.php
@@ -11,11 +11,11 @@ require 'vendor/autoload.php';
 
 use Aws\Rds\RdsClient;
 $client = RdsClient::factory(array(
-'region'  => 'us-east-1'
+'region'  => 'us-west-2'
 ));
 
 $result = $client->describeDBInstances(array(
-    'DBInstanceIdentifier' => 'itmo544jrhdb',
+    'DBInstanceIdentifier' => 'Project1db',
 ));
 
 $endpoint = "";
@@ -26,7 +26,7 @@ foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep) {
     $endpoint = $ep;
 }   
 //echo "begin database";
-$link = mysqli_connect($endpoint,"controller","ilovebunnies","itmo544db") or die("Error " . mysqli_error($link));
+$link = mysqli_connect($endpoint,"nandini","nandinipwd","Project1db") or die("Error " . mysqli_error($link));
 
 /* check connection */
 if (mysqli_connect_errno()) {
@@ -35,8 +35,8 @@ if (mysqli_connect_errno()) {
 }
 
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
-$link->real_query("SELECT * FROM items WHERE email = '$email'");
-//$link->real_query("SELECT * FROM items");
+$link->real_query("SELECT * FROM Projectrec WHERE email = '$email'");
+//$link->real_query("SELECT * FROM Projectrec");
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {

ea3e136dfe0ef5bdffafad59ed5d1aa595fed208
nandini <nandini9590@gmail.com>
2015-11-06 23:09:09 -0600

    displaying all images

--- gallery.php
+++ gallery.php
@@ -32,12 +32,12 @@ if (mysqli_connect_errno()) {
 }
 
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
-$link->real_query("SELECT * FROM Projectrec WHERE email = '$email'");
+$link->real_query("SELECT * FROM Projectrec");
 //$link->real_query("SELECT * FROM Projectrec");
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
-    echo "<img src =\" " . $row['raws3url'] . "\" /><img src =\"" .$row['finisheds3url'] . "\"/>";
+    echo "<img src =\" " . $row['raws3url'] . "\" />";
 echo $row['id'] . "Email: " . $row['email'];
 }
 $link->close();
